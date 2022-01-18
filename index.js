50c6602592816c45c3b937f22734dd242c42b012
isaacradford <iradford@byui.edu>
2022-01-17 18:54:10 -0700

    week 2 assignment

---
+++ index.js
@@ -0,0 +1,8 @@
+let currentdate = document.lastModified
+document.write("Last Modified: " +currentdate)
+
+// todays_date = document.getElementById ('modified').textContent = `Last Updated ${currentdate} .`;
+
+// let currentdate = document.lastModified;
+// // document.getElementById ('modified').textContent = 'Last UpdatedL '+ currentdate + ' .';
+// document.getElementById ('modified').textContent = `Last Updated ${currentdate} .`;
