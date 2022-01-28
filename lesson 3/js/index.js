689f0dfc9c2a552efce7790642982b5caba3430d
isaacradford <iradford@byui.edu>
2022-01-27 22:40:04 -0700

    small adjustments

---
+++ lesson 3/js/index.js
@@ -0,0 +1,2 @@
+let currentdate = document.lastModified
+document.write("Last Modified: " +currentdate)
--- lesson 3/practice/practice/scripts/scripts.js
+++ lesson 3/practice/practice/scripts/scripts.js
@@ -1,7 +1,24 @@
 const button = document.querySelector('.btn');
+// const buttons = document.getElementByClassName('btn');
+
 
 function show() {
-    button.classList.add('newstyle');
+    // button.classList.add('newstyle');
+    button.classList.toggle('newstyle');
+    // it will be a toggle button for the hamburger menu
 };
 
 button.addEventListener('click', show);
+// buttons.addEventListener('click', show);
+
+let w_names = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+let m_names = ['January', 'February', 'March', 'April']
+
+let mydate = new Date();
+console.log(mydate);
+
+let weekname = w_names[mydate.getDay()]
+console.log(mydate.getDay());
+let monthname = m_names[mydate.getMonth()];
+
+const dateoutput = document.querySelector('#date')
