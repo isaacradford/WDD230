8264afe54b7c73a4a6c50bdd41e08d7c04d9d891
isaacradford <iradford@byui.edu>
2022-03-21 21:39:27 -0600

    s

--- lesson 4/js/scripts.js
+++ lesson 4/js/scripts.js
@@ -172,3 +172,41 @@ function display_data_by(format) {
   
   
 
+// Weather API
+
+const apiURL = 'https:api.openweathermap.org/data/2.5/weather?q=Rexburg,us&units=imperial&appid=c515a57b84161fb3f31f685e290f0729'
+
+fetch (apiURL)
+  .then ((response) => response.json())
+  .then((jsObject) => {
+    console,log(jsObject)
+    weatherBookedScript(jsObject)
+    getWindchill(jsObject)
+  });
+
+function weather (jsObject) {
+  document.querySelector('#weather-temp').textContent = jsObject.main.temp;
+  const iconsrc = `` 
+  // iconsrc is the image for the html
+  const desc = jsObject.weather[0].description;
+  document.querySelector('#weathericon').setAttribute('src', iconsrc)
+  document.querySelector('#weathericon').setAttribute('alt', desc)
+  document.querySelector('figcaption').textContent = desc;
+  document.querySelector('#wind-speed').textContent = jsObject.wind.speed;
+}
+
+function getWindChill (jsObject) {
+  let temperature = document.querySelector('#weather-temp')
+  let windspeed = document.querySelector('#wind-speed')
+  let windchill = document.querySelector('#wind-chill')
+
+  temperature = jsObject.main.temp;
+  windspeed = jsObject.wind.speed;
+
+  if (temperature > 50 || windspeed < 3) {
+    windchill.textContent = 'N/A'
+  } else {
+    let chill = Math.round(35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windspeed, 0.16)) + (0.4275 * temperature * Math.pow(windspeed, 0.16)));
+    windchill.textContent = `${chill}`
+  }
+}
